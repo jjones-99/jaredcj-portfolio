344eb9e1cc7a5b51194e9559bc406f90502db55d
Jared Jones <jared@jaredcj.com>
2021-07-11 22:22:57 -0600

    Add font to header.

--- src/components/header/HeaderStyles.tsx
+++ src/components/header/HeaderStyles.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const HeaderDiv = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 0.5rem;
   height: ${props => props.theme.headerHeight};
+  font: ${(props) => props.theme.fontText}
 `;
