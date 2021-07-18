520993e511a5c57c176cbce80df20403cbe9c66c
Jared Jones <jared@jaredcj.com>
2021-07-17 19:01:35 -0600

    Install three/tailwind.

---
+++ src/components/three-canvas/ThreeCanvas.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Canvas } from "./ThreeCanvasStyles";
+
+const ThreeCanvas: React.FC = () => {
+  return <Canvas className="three-canvas"></Canvas>;
+};
+
+export default ThreeCanvas;
---
+++ src/components/three-canvas/ThreeCanvasStyles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Canvas = styled.canvas`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -100;
+`;
