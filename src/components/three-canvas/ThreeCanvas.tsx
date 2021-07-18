740b7d99ae4ad2de5247b9992dd9dd25bc86d4d4
Jared Jones <jared@jaredcj.com>
2021-07-17 20:21:24 -0600

    Hidden background for three.js.

--- src/components/three-canvas/ThreeCanvas.tsx
+++ src/components/three-canvas/ThreeCanvas.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { Canvas } from "./ThreeCanvasStyles";
 import * as THREE from "three";
+import { render } from "@testing-library/react";
+
+const PIXEL_RATIO = window.devicePixelRatio;
 
 const ThreeCanvas: React.FC = () => {
   useEffect(() => {
@@ -10,33 +13,32 @@ const ThreeCanvas: React.FC = () => {
     // ===== Helpers for three.js =====
 
     const getHeight = () => canvas.offsetHeight;
-    const getWidth = () => canvas.offsetHeight;
+    const getWidth = () => canvas.offsetWidth;
 
     // ===== START three.js =====
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(
-      75,
-      getHeight() / getWidth(),
-      0.1,
-      1000
-    );
-    var renderer = new THREE.WebGLRenderer({
+    const scene = new THREE.Scene();
+    const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
+      antialias: true,
+      alpha: true,
     });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    renderer.setSize(getWidth(), getHeight());
+    renderer.setPixelRatio(PIXEL_RATIO);
+    const camera = new THREE.PerspectiveCamera(75, getWidth() / getHeight(), 0.1, 1000);
     camera.position.z = 5;
-    var animate = function () {
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    const animate = () => {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
       renderer.render(scene, camera);
     };
-
     animate();
 
     // ===== END three.js =====
