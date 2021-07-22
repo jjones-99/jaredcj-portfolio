7dd4e070535ba550d628004806298ece4ca55c47
Jared Jones <jared@jaredcj.com>
2021-07-21 18:53:18 -0600

    Create Playground component.

---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PlaygroundContainer } from "./PlaygroundStyles";
+
+const Playground = () => {
+  return <PlaygroundContainer>Playground</PlaygroundContainer>;
+};
+
+export default Playground;
