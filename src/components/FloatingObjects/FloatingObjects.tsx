c56595b395adf245ab25fe010f1943c58c5b89f8
Jared Jones <jared@jaredcj.com>
2021-07-22 22:14:40 -0600

    Animate stripes.

--- src/components/FloatingObjects/FloatingObjects.tsx
+++ src/components/FloatingObjects/FloatingObjects.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BarL, BarM, BarS, ObjectFullSizeWrapper, ObjectWrapper } from "./FloatingObjectsStyles";
+import gsap from "gsap";
+
+interface Position {
+  x?: number;
+  y?: number;
+}
 
 const FloatingObjects = () => {
+  const [mouse, setMouse] = useState<Position>({
+    x: undefined,
+    y: undefined,
+  });
+  const wrapperElement = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("pointermove", (event) => {
+      setMouse({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!wrapperElement.current) return;
+    gsap.to(wrapperElement.current, { x: mouse.x, y: mouse.y, stagger: -0.1 });
+  }, [mouse]);
+
   return (
     <ObjectFullSizeWrapper>
-      <ObjectWrapper>
+      <ObjectWrapper ref={wrapperElement}>
         <BarL />
         <BarM />
         <BarS />
