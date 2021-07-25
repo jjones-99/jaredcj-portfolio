97174aff0af25084c6c4cfda5ea4b650fa9e5481
Jared Jones <jared@jaredcj.com>
2021-07-25 14:11:49 -0600

    Implement basic navbar for thin screen.

--- src/components/Navbar/ThinNavbar.tsx
+++ src/components/Navbar/ThinNavbar.tsx
@@ -1,16 +1,34 @@
-import React from "react";
-import { ThinNavbarItem, ThinNavbarNav } from "./ThinNavbarStyles";
+import React, { useState } from "react";
+import {
+  ThinNavbarButton,
+  ThinNavbarHeader,
+  ThinNavbarItem,
+  ThinNavbarMenu,
+  ThinNavbarNav,
+} from "./ThinNavbarStyles";
 
 /**
  * Represents site navigation for the portfolio.
  */
 const ThinNavbar: React.FC = () => {
+  const [renderMenu, setRenderMenu] = useState(false);
+
   return (
-    <ThinNavbarNav>
-      <ThinNavbarItem href="#work">asdf</ThinNavbarItem>
-      <ThinNavbarItem href="#playground">Playground</ThinNavbarItem>
-      <ThinNavbarItem href="#contact">Contact</ThinNavbarItem>
-    </ThinNavbarNav>
+    <>
+      <ThinNavbarButton onClick={() => setRenderMenu(true)}>Menu</ThinNavbarButton>
+      {renderMenu && (
+        <ThinNavbarMenu>
+          <ThinNavbarHeader>
+            <ThinNavbarButton onClick={() => setRenderMenu(false)}>Close</ThinNavbarButton>
+          </ThinNavbarHeader>
+          <ThinNavbarNav>
+            <ThinNavbarItem onClick={() => setRenderMenu(false)} href="#work">Work</ThinNavbarItem>
+            <ThinNavbarItem onClick={() => setRenderMenu(false)} href="#playground">Playground</ThinNavbarItem>
+            <ThinNavbarItem onClick={() => setRenderMenu(false)} href="#contact">Contact</ThinNavbarItem>
+          </ThinNavbarNav>
+        </ThinNavbarMenu>
+      )}
+    </>
   );
 };
 
--- src/components/Navbar/ThinNavbarStyles.ts
+++ src/components/Navbar/ThinNavbarStyles.ts
@@ -1,6 +1,33 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+export const ThinNavbarButton = styled.button`
+  border: none;
+  background: none;
+`;
+
+export const ThinNavbarMenu = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: white;
+  height: 100vh;
+`;
+
+export const ThinNavbarHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 0.75rem;
+  height: 10%;
+`;
 
 export const ThinNavbarNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 90%;
+  padding: 30% 30px 60% 30px;
 `;
 
 export const ThinNavbarItem = styled.a`
@@ -9,9 +36,10 @@ export const ThinNavbarItem = styled.a`
   color: black;
   text-decoration: none;
   font-weight: 500;
-  margin-right: 1rem;
+  font-size: 3rem;
+  margin-bottom: 1rem;
 
   &:last-child {
-    margin-right: 0;
+    margin-bottom: 0;
   }
 `;
