87f48e53bcd1924a73002441521b6fb9edf26eaa
Jared Jones <jared@jaredcj.com>
2021-08-01 21:42:30 -0600

    Update experiment copy.

--- src/components/Playground/PlayItems.ts
+++ src/components/Playground/PlayItems.ts
@@ -1,5 +1,5 @@
-import ProjectCards from "../../assets/experiments/ProjectCards.gif";
-import ShiningText from "../../assets/experiments/ShiningText.gif";
+import ProjectCardsThumbnail from "../../assets/experiments/ProjectCards.gif";
+import ShiningTextThumbnail from "../../assets/experiments/ShiningText.gif";
 
 /**
  * Defines the information an experiment/playground item should have.
@@ -22,23 +22,21 @@ export const PLAY_ITEMS: PlayItem[] = [
   {
     title: "Shining Text",
     time: "2021",
-    thumbnailSrc: ShiningText,
+    thumbnailSrc: ShiningTextThumbnail,
     gallerySrcs: [],
     description: `Learning how to use three.js with textures and shaders.`,
     tags: ["TypeScript", "Vite", "three.js", "shaders"],
     codepenLink: "https://codepen.io/jaredcj/full/ZEKBmMQ",
     githubLink: "https://github.com/jjones-99/shining-text",
-
   },
   {
     title: "These Cards",
     time: "2021",
-    thumbnailSrc: ProjectCards,
+    thumbnailSrc: ProjectCardsThumbnail,
     gallerySrcs: [],
-    description: `Experimenting with using three.js in React with three dimensions.`,
+    description: `Designing/developing cards with HTML/CSS to represent experiments on my portfolio site.`,
     tags: ["TypeScript", "React", "three.js"],
     codepenLink: "https://codepen.io/jaredcj/full/mdmKdQB",
-    githubLink: "",
   },
   // {
   //   title: "Icon Field",
