eeecc14234620ccf150cc1c6164ecb2683374cfc
Jared Jones <jared@jaredcj.com>
2021-08-01 22:40:42 -0600

    Change ParticleBackground to be full screen.

--- src/components/Particles/ParticleBackground.tsx
+++ src/components/Particles/ParticleBackground.tsx
@@ -4,24 +4,6 @@ import { ParticleBackgroundWrapper } from "./ParticleStyles";
 
 const PARTICLE_OPTIONS: RecursivePartial<IOptions> = {
   fpsLimit: 60,
-  interactivity: {
-    detectsOn: "canvas",
-    modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 40,
-      },
-      push: {
-        quantity: 4,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
   particles: {
     color: {
       value: "#333333",
@@ -34,7 +16,7 @@ const PARTICLE_OPTIONS: RecursivePartial<IOptions> = {
       enable: true,
       outMode: "bounce",
       random: false,
-      speed: 20.2,
+      speed: 0.2,
       straight: false,
     },
     number: {
--- src/components/Particles/ParticleStyles.ts
+++ src/components/Particles/ParticleStyles.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const ParticleBackgroundWrapper = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   left: 0;
-  height: 200vh;
+  height: 0;
   z-index: -1;
 `;
