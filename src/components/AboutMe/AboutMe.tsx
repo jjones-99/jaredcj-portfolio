a3fb8f0bab679f3b01818238f3687c51e76a5def
Jared Jones <jared@jaredcj.com>
2021-08-03 18:40:13 -0600

    Creating the about me section.

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import { AboutMeContainer } from './AboutMeStyles'
+import React from "react";
+import {
+  AboutMeBody,
+  AboutMeContainer,
+  AboutMeHeader,
+  AboutMeInfo,
+  AboutMeList,
+  AboutMeListItem,
+  AboutMeMain,
+  AboutMeSection,
+  AboutMeTopic,
+} from "./AboutMeStyles";
 
 const AboutMe = () => {
   return (
     <AboutMeContainer>
-      The about me card will go here. Is frosted glass too much?
+      <AboutMeMain>
+        <AboutMeBody>
+          <p>I'm Jared!</p>
+          <p>I'm passionate about tidy code and design. Things that are easy to maintain improve productivity and positivity.</p>
+          <p>I'm always looking to learn more about what I don't know and improve upon what I do.</p>
+        </AboutMeBody>
+        <AboutMeSection>
+          <AboutMeHeader>I do front-end development.</AboutMeHeader>
+          <AboutMeTopic>Languages I speak</AboutMeTopic>
+          <AboutMeInfo>HTML, CSS, SCSS, JavaScript, TypeScript</AboutMeInfo>
+          <AboutMeTopic>Tools I know</AboutMeTopic>
+          <AboutMeInfo>
+            React, Angular, three.js, GSAP, Bootstrap, Tailwind, GitHub, SharePoint, WordPress
+          </AboutMeInfo>
+        </AboutMeSection>
+        <AboutMeSection>
+          <AboutMeHeader>I do back-end development.</AboutMeHeader>
+          <AboutMeTopic>Languages I speak</AboutMeTopic>
+          <AboutMeInfo>Java, JavaScript, C, C++, Python</AboutMeInfo>
+          <AboutMeTopic>Tools I know</AboutMeTopic>
+          <AboutMeInfo>Node.js, SQL, NoSQL, JPA, RESTful</AboutMeInfo>
+        </AboutMeSection>
+        <AboutMeSection>
+          <AboutMeHeader>I want to learn about design.</AboutMeHeader>
+          <AboutMeTopic>Tools I know</AboutMeTopic>
+          <AboutMeInfo>Adobe XD, Figma</AboutMeInfo>
+        </AboutMeSection>
+      </AboutMeMain>
     </AboutMeContainer>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/AboutMe/AboutMeStyles.ts
+++ src/components/AboutMe/AboutMeStyles.ts
@@ -1,8 +1,60 @@
 import styled from "styled-components";
 
 export const AboutMeContainer = styled.section`
-  height: 40vh;
+  min-height: 40vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+
+export const AboutMeMain = styled.article`
+  width: 80%;
+  background-color: gray;
+  border-radius: 2rem;
+  font: ${({ theme }) => theme.fontText};
+`;
+
+export const AboutMeBody = styled.section`
+  padding: 2rem;
+  border-radius: 2rem 2rem 0 0;
+  background-color: ${({ theme }) => theme.colorPrimary};
+  text-align: center;
+  color: white;
+`;
+
+export const AboutMeSection = styled.section`
+  position: relative;
+  padding: 2rem;
+  text-align: center;
+
+  &:before {
+    --border-width: 50%;
+    content: "";
+    position: absolute;
+    left: calc(var(--border-width) / 2);
+    bottom: 0;
+    height: 1px;
+    width: var(--border-width);
+    border-bottom: 2px solid dimgrey;
+  }
+
+  &:last-child:before {
+    border-bottom: none;
+  }
+`;
+
+export const AboutMeHeader = styled.h5``;
+
+export const AboutMeTopic = styled.h6`
+  font-weight: 500;
+`;
+
+export const AboutMeInfo = styled.p``;
+
+export const AboutMeList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+export const AboutMeListItem = styled.li``;
